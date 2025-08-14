005834a670bf897463624fe8618d590f7390246e
mohit2709-repo <sharma.mohit332@gmail.com>
2025-08-14 20:38:16 +0530

    solution70

---
+++ Solution70.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n ==2 :
+            return 2
+
+        total = [0] * (n-1)
+        total[1] = 1
+        total[2] = 2
+
+        for i in range (3, n+1):
+            total[i] = total[i-1] + total[i-2]
+
+        return total[i]
