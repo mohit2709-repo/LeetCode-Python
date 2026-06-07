5c849905513b7d96f039c2bd233e8844680c9700
mohit2709-repo <sharma.mohit332@gmail.com>
2026-06-07 17:45:29 +0530

    Python solutions

---
+++ Arrays_Hashing/k_frequent_element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def is_anagram(self, nums: list[int], k: int) -> list[int]:
+        count = {}
+        frequency = [[]for i in range(len(nums)+ 1)]
+
+        for n in nums:
+            count = 1 + count.get(n, 0)
+
+        for n, c in count.items():
+            frequency[c].append(n)
+        
+        res = []
+        for i in range (len(frequency) - 1, 0, -1):
+            for n in frequency[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
